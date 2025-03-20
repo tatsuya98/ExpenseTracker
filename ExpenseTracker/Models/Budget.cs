a8648abc5db46c46286f3abab31440fa613eac20
tatsuya98 <nathanbailey_62@hotmail.com>
2025-03-20 14:05:08 +0000

    models done

--- ExpenseTracker/Models/Budget.cs
+++ ExpenseTracker/Models/Budget.cs
@@ -1,12 +1,14 @@
-﻿namespace ExpenseTracker.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
 {
     public class Budget
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public int CategoryId { get; set; }
+        [Column(TypeName = "decimal(9,2)")]
         public decimal Amount { get; set; }
-        public Category Category { get; set; }
+        public string Description { get; set; }
         public User User { get; set; }
     }
 
--- ExpenseTracker/Models/Expense.cs
+++ ExpenseTracker/Models/Expense.cs
@@ -1,11 +1,16 @@
-﻿namespace ExpenseTracker.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
 {
     public class Expense
     {
         public int Id { get; set; }
+        [Column(TypeName = "decimal(9,2)")]
         public decimal Amount { get; set; }
         public DateTime ExpenseDate { get; set; } = DateTime.Now.ToUniversalTime();
         public int CategoryId { get; set; }
+        public int UserId { get; set; }
         public Category Category { get; set; }
+        public User User { get; set; }
     }
 }
